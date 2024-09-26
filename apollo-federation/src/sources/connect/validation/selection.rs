8a5807a00243274db33f034cb8c93626b57fbd98
Matthew Hawkins <matthew.hawkins@apollographql.com>
2024-09-26 10:27:57 -0600

    Resolve merge conflicts from next

--- apollo-federation/src/sources/connect/validation/selection.rs
+++ apollo-federation/src/sources/connect/validation/selection.rs
@@ -28,10 +28,10 @@ use crate::sources::connect::json_selection::NamedSelection;
 use crate::sources::connect::json_selection::PathList;
 use crate::sources::connect::spec::schema::CONNECT_SELECTION_ARGUMENT_NAME;
 use crate::sources::connect::validation::coordinates::connect_directive_http_body_coordinate;
+use crate::sources::connect::validation::graphql::SchemaInfo;
 use crate::sources::connect::validation::selection::visitor::visit;
 use crate::sources::connect::validation::selection::visitor::SelectionPart;
 use crate::sources::connect::validation::selection::visitor::Visitor;
-use crate::sources::connect::validation::graphql::SchemaInfo;
 use crate::sources::connect::JSONSelection;
 use crate::sources::connect::SubSelection;
 
